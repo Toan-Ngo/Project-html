98f0bb5efd42d628b305d6855d0f9f6fa7974dc4
Toanngo <mttoan7@gmail.com>
2025-09-05 17:30:28 +0700

    Project-4

---
+++ Project-4.html/script.js
@@ -0,0 +1,136 @@
+// book class: đại diện 1 cuốn sách
+class Book{
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+// UI CLASS : GIAO DIỆN
+
+class UI{
+    static displayBooks(){
+
+        const books = Store.getBooks();
+
+        books.forEach(book => {
+            UI.addBookToList(book)
+        });
+    }
+    static addBookToList(book){
+        const list = document.querySelector("#book-list");
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href = "#" class="btn btn-danger btn-sm delete">X</a></td>
+        `
+
+        list.appendChild(row);
+    }
+
+    static deleteBook(el){
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlerts(message, className) {
+        const div = document.createElement('div');
+
+        div.className = `alert alert-${className}`;
+
+        div.appendChild(document.createTextNode(message));
+
+
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+
+        container.insertBefore(div, form);
+
+        //Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 2000);
+
+
+    }
+
+    static clearFields(){
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+
+}
+
+// STORE CLASS: LƯU TRỮ
+
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }
+        else{
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static addBooks(book){
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach((book,index) => {
+            if(book.isbn === isbn){
+                books.splice(index,1);
+            }
+        });
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+}
+
+// EVENTS: HIỆN THỊ SÁCH
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// EVENT: Thêm sách
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    if(title === '' || author === '' || isbn === '' ){
+        UI.showAlerts('Please fill in the fields','danger');
+    }
+    else{
+        const book = new Book(title,author,isbn);
+
+        UI.showAlerts('Book Added','success');
+
+        UI.addBookToList(book);
+
+        Store.addBooks(book);
+
+        UI.clearFields();
+    }
+
+    
+});
+
+// EVENT: XÓA 1 CUỐN SÁCH
+document.querySelector("#book-list").addEventListener('click', (e) =>{
+    UI.deleteBook(e.target);
+
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+    UI.showAlerts('Book Removed', 'success');
+})
