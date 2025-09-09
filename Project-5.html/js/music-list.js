ed7a108dfdaa9f7b8ebd065aa6ebbfa429a8fcab
Toanngo <mttoan7@gmail.com>
2025-09-09 22:05:47 +0700

    Project_5

---
+++ Project-5.html/js/music-list.js
@@ -0,0 +1,19 @@
+let allMusic =[{
+    name: "Anh vui",
+    artist: "Phạm kỳ",
+    img: "anh-vui",
+    src:"Anh_vui"
+},
+{
+    name: "Ừ có anh đây",
+    artist: "Tino",
+    img: "u-co-anh-day",
+    src:"U_Co_Anh_Day" 
+},
+{
+    name: "Sợ phải kết thúc",
+    artist: "Nhật Minh",
+    img: "so-phai-ket-thuc",
+    src:"So_Phai_Ket_Thuc" 
+}
+]
